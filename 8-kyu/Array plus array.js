bbb5f971d368c7d0a4eec47b5770e44d357db5d1
V-Sugak <vicka.sugak2010@yandex.ru>
2021-10-03 12:03:59 +0300

    Array plus array

---
+++ 8-kyu/Array plus array.js
@@ -0,0 +1,8 @@
+function arrayPlusArray(arr1, arr2) {
+    let arr = arr1.concat(arr2);
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum
+}
