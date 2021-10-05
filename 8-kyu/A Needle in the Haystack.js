1bcf5d6afef2e571f44f3ba00ae65ca1ca56f3e8
V-Sugak <vicka.sugak2010@yandex.ru>
2021-10-05 20:14:36 +0300

    A Needle in the Haystack

---
+++ 8-kyu/A Needle in the Haystack.js
@@ -0,0 +1,11 @@
+function findNeedle(haystack) {
+    return "found the needle at position " + haystack.indexOf("needle")
+}
+
+function findNeedle(haystack) {
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === 'needle') {
+            return `found the needle at position ${i}`
+        }
+    }
+}
