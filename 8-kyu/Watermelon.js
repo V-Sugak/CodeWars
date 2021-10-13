c779afa6b1d2e3f4ac13728c6b8596790f79e0f7
V-Sugak <vicka.sugak2010@yandex.ru>
2021-10-13 22:49:20 +0300

    Watermelon

---
+++ 8-kyu/Watermelon.js
@@ -0,0 +1,9 @@
+function divide(weight) {
+    return (weight >= 4 && weight % 2 === 0)
+}
+
+function divide(weight) {
+    if (weight === 2) { return false }
+    else if (weight % 2 === 0) { return true }
+    else { return false }
+}
