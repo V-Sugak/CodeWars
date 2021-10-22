5a36491a153b67daa6c5d937a8ac57e6016dedbb
V-Sugak <vicka.sugak2010@yandex.ru>
2021-10-22 21:29:45 +0300

    Thinkful - Logic Drills: Traffic light

---
+++ 8-kyu/Thinkful - Logic Drills Traffic light.js
@@ -0,0 +1,10 @@
+function updateLight(current) {
+    return current === 'yellow' ? 'red' :
+        current === 'green' ? 'yellow' : 'green';
+}
+
+function updateLight(current) {
+    if (current === "green") { return "yellow" }
+    else if (current === "yellow") { return "red" }
+    else if (current === "red") { return "green" }
+}
