2b0fc500e52b96fa091568c885560d9914f2bac2
V-Sugak <vicka.sugak2010@yandex.ru>
2021-11-13 11:54:55 +0300

    Multiples of 3 or 5

---
+++ 6-kyu/Multiples of 3 or 5.js
@@ -0,0 +1,10 @@
+function solution(number) {
+    if (number <= 0) { return 0 }
+    else {
+        let result = 0;
+        for (let i = 3; i < number; i++) {
+            if (i % 3 === 0 || i % 5 === 0) { result += i; }
+        }
+        return result;
+    }
+}
