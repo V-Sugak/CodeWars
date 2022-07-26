c6d82e962edb62c72952f877a207b5c88a61f504
V-Sugak <vicka.sugak2010@yandex.ru>
2022-07-26 22:19:57 +0300

    Find the next perfect square!

---
+++ 7-kyu/Find the next perfect square!.js
@@ -0,0 +1,5 @@
+function findNextSquare(sq) {
+
+    // Return the next square if sq is a perfect square, -1 otherwise
+    return Number.isInteger(Math.sqrt(sq)) ? Math.pow(Math.sqrt(sq) + 1, 2) : -1;
+}
